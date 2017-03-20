2f95dab96c26cf7fc7a7067017ddbf0c4da6603e
Dan Holtby <djholtby@gmail.com>
2017-03-20 17:30:51 -0400

    about time to check these in

--- src/racket/griftos/telnet.rkt
+++ src/racket/griftos/telnet.rkt
@@ -17,7 +17,7 @@
  | A Telnet is a C-Pointer (telnet *)                                          |
  | (native-register telnet socket) registers the native telnet* with the Racket|
  |   socket object (hooks the callbacks so telnet messages are relayed properly|
- |
+ |                                                                             |
  | (native-send telnet bytes) sends a bytestring to the given telnet socket    |
  |                                                                             |
  | (native-gmcp telnet key bytes) sends a gmcp message over the given telnet   |
@@ -36,8 +36,8 @@
   (init-field native-socket)
   (link-native-socket native-socket this)
   (field [gmcp (make-hash)]
-         [on-gmcp (void)]
-         [on-text (void)])
+         [on-gmcp void]
+         [on-text void])
   
   (define/public (gmcp-send key value)
     (unless (jsexpr? value) (raise-argument-error 'gmcp-send "jsexpr?" value))
@@ -45,6 +45,6 @@
   
   (define/public (gmcp-receive key value)
     (hash-set! gmcp key (bytes->jsexpr value))
-    (when (procedure? on-gmcp) (on-gmcp gmcp)))
+    (on-gmcp gmcp))
   
   ))
