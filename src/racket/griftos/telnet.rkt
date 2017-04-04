e56a3eb4650cecc83677bfff50a08575cbeb9dc9
Dan Holtby <djholtby@gmail.com>
2017-04-03 20:40:30 -0400

    assorted updates to racket code

--- src/racket/griftos/telnet.rkt
+++ src/racket/griftos/telnet.rkt
@@ -31,20 +31,29 @@
 
 
 
-(define telnet-socket (class object%
+(define telnet-socket% (class object%
   (super-new)
   (init-field native-socket)
-  (link-native-socket native-socket this)
+;  (link-native-socket native-socket this)
   (field [gmcp (make-hash)]
          [on-gmcp void]
          [on-text void])
   
   (define/public (gmcp-send key value)
-    (unless (jsexpr? value) (raise-argument-error 'gmcp-send "jsexpr?" value))
+    (unless (jsexpr? value) (raise-argument-error 'telnet-socket:gmcp-send "jsexpr?" value))
     (native-gmcp-send native-socket key (jsexpr->bytes value)))
   
   (define/public (gmcp-receive key value)
     (hash-set! gmcp key (bytes->jsexpr value))
     (on-gmcp gmcp))
+
+  (define/public (send msg)
+    (unless (or (string? msg) (bytes? msg))
+      (raise-argument-error 'telnet-socket:send "(or string? bytes?)" value))
+    (native-send native-socket (if (string? msg) (string->bytes/utf-8 msg) msg)))
+
+  (define/public (rec msg)
+    (on-msg (bytes->string/utf-8 msg)))
+
   
   ))
