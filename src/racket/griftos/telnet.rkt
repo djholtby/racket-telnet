b035deff355c88c14f1ee263102bcf760f242753
Dan Holtby <djholtby@gmail.com>
2018-02-14 08:21:04 -0500

    major rewrite of libevent backend (sync instead of async)

--- src/racket/griftos/telnet.rkt
+++ src/racket/griftos/telnet.rkt
@@ -1,59 +1,55 @@
 #lang racket
-(require racket/class
-         json)
+(require racket/class json)
+(require "transcode.rkt")
 
-#|-----------------------------------------------------------------------------
- | Racket Telnet interface module
- | 
- |
- |
- |
- |-----------------------------------------------------------------------------|#
+(provide telnet-user telnet-user? telnet-user-cptr telnet-user-settings telnet-user-th set-telnet-user-th! telnet-user-out telnet-user-conn? set-telnet-user-conn?!
+         new-connection send-to-user register-print-callback)
 
-
-;(require native/telnet)
-#|-----------------------------------------------------------------------------|
- | Native interface                                                            |
- | A Telnet is a C-Pointer (telnet *)                                          |
- | (native-register telnet socket) registers the native telnet* with the Racket|
- |   socket object (hooks the callbacks so telnet messages are relayed properly|
- |                                                                             |
- | (native-send telnet bytes) sends a bytestring to the given telnet socket    |
- |                                                                             |
- | (native-gmcp telnet key bytes) sends a gmcp message over the given telnet   |
- |    socket, using the bytestring key and JSON message content bytes          |
- |                                                                             |
- | (native-tel-printf telnet fmt ...) sends a C style printf message to telnet |
- |                                                                             |
- | (native-tel-rprintf telnet fmt ...) sends a Racket style printf message     |
- |-----------------------------------------------------------------------------|#
+; (send-to-user cptr bytes) sends bytes to the C backend telnet struct cptr
+; send-to-user: CPTR ByteStr -> Void
+(define send-to-user #f)
 
 
+; (register-print-callback cb) registers native function cb as the send-to-user function
+; register-print-callback: (CPTR ByteStr -> Void) -> Void
+(define (register-print-callback cb)
+  (unless (procedure? cb)
+    (raise-argument-error 'register-print-callback "procedure?" cb))
+  (unless (procedure-arity-includes? cb 2)
+    (raise-argument-error 'register-print-callback "procedure-arity-includes? 2" cb))
+  (set! send-to-user cb))
+
+;; supports: (listof Sym)
+;; encoding: Sym
+;; to-utf8 : Bytes-Converter
+;; from-utf8 : Bytes-Converter
+
+(struct telnet-settings (supports encoding to-utf8 from-utf8) #:mutable)
+
+(struct telnet-user
+  ( cptr     ; int?              - cptr cast as uintptr_t for speed reasons
+    [th #:mutable]      ; 
+    settings; telnet-settings?
+    out     ; output-port?      - printing to here wraps it up and sends it to C backend
+    [conn? #:mutable]    ; boolean?          - is currently connected
+    )
+  #:property prop:output-port (struct-field-index out))
 
 
-(define telnet-socket% (class object%
-  (super-new)
-  (init-field native-socket)
-;  (link-native-socket native-socket this)
-  (field [gmcp (make-hash)]
-         [on-gmcp void]
-         [on-text void])
-  
-  (define/public (gmcp-send key value)
-    (unless (jsexpr? value) (raise-argument-error 'telnet-socket:gmcp-send "jsexpr?" value))
-    (native-gmcp-send native-socket key (jsexpr->bytes value)))
-  
-  (define/public (gmcp-receive key value)
-    (hash-set! gmcp key (bytes->jsexpr value))
-    (on-gmcp gmcp))
 
-  (define/public (send msg)
-    (unless (or (string? msg) (bytes? msg))
-      (raise-argument-error 'telnet-socket:send "(or string? bytes?)" value))
-    (native-send native-socket (if (string? msg) (string->bytes/utf-8 msg) msg)))
+(define (new-connection cptr)
+  (define default-encoding 'ISO-8859-1)
+  (define default-settings
+    (telnet-settings '() default-encoding (get-converter #f default-encoding) (get-converter #t default-encoding)))
+  (telnet-user cptr
+               #f
+               default-settings
+               (make-output-port 'telnet-writer ; name
+                                 always-evt ; evt
+                                 (lambda (bytes start end non-block? breakable?) ; write-out, never blocks because the channel is non-blocking
+                                   (send-to-user cptr (bytes-convert/complete (subbytes bytes start end) (telnet-settings-from-utf8 default-settings)))
+                                   (- end start))
+                                 void)
+               #t
+               ))
 
-  (define/public (rec msg)
-    (on-msg (bytes->string/utf-8 msg)))
-
-  
-  ))
