01459dce7d82939ef392dedf052341841a8275ba
Dan Holtby <djholtby@gmail.com>
2018-02-22 14:16:31 -0500

    MSDP MSSP etc. support

--- src/racket/griftos/telnet.rkt
+++ src/racket/griftos/telnet.rkt
@@ -1,13 +1,42 @@
 #lang racket
 (require racket/class json)
-(require "transcode.rkt")
+(require "objects.rkt")
+(require "msdp.rkt")
 
-(provide telnet-user telnet-user? telnet-user-cptr telnet-user-settings telnet-user-th set-telnet-user-th! telnet-user-out telnet-user-conn? set-telnet-user-conn?!
-         new-connection send-to-user register-print-callback)
+(provide telnet-message?/c telnet-message? telnet-send telnet-send/gmcp telnet-send/msdp telnet-set-echo
+         register-print-callback add-message-to-griftos)
 
-; (send-to-user cptr bytes) sends bytes to the C backend telnet struct cptr
-; send-to-user: CPTR ByteStr -> Void
-(define send-to-user #f)
+(provide telnet telnet? telnet?/c telnet-cptr telnet-encoding set-telnet-encoding! telnet-client set-telnet-client! telnet-term
+         set-telnet-term! telnet-supports set-telnet-supports! telnet-supports? telnet-connected? set-telnet-connected?!
+         telnet-user-data set-telnet-user-data!)
+
+;; a telnet message is one of
+;; * Bytes
+;; * (list 'GMCP Bytes)
+;; * (list 'MSSP Bytes)
+;; * EOF -- indicates a server-side disconnect request
+
+(define telnet-message?/c (or/c bytes?
+                                string?
+                                (cons/c symbol? (or/c bytes? (cons/c exact-nonnegative-integer? exact-nonnegative-integer?)))
+                                eof-object?))
+
+(define (telnet-message? m)
+  (or (bytes? m)
+      (string? m)
+      (and (cons? m)
+           (symbol? (car m))
+           (or (bytes? (cdr m))
+               (and (cons? cdr m)
+                    (exact-nonnegative-integer? (cadr m))
+                    (exact-nonnegative-integer? (cddr m)))))
+               
+      (eof-object? m)))
+
+; (send-to-user t message) sends message to the C backend telnet struct cptr
+; send-to-user: Telnet TelnetMessage -> Void
+
+(define telnet-send void)
 
 
 ; (register-print-callback cb) registers native function cb as the send-to-user function
@@ -17,39 +46,70 @@
     (raise-argument-error 'register-print-callback "procedure?" cb))
   (unless (procedure-arity-includes? cb 2)
     (raise-argument-error 'register-print-callback "procedure-arity-includes? 2" cb))
-  (set! send-to-user cb))
+  (set! telnet-send
+        (lambda (t msg)
+          (cb (telnet-cptr t) msg))))
 
-;; supports: (listof Sym)
-;; encoding: Sym
-;; to-utf8 : Bytes-Converter
-;; from-utf8 : Bytes-Converter
+(define (telnet-send/gmcp t key data)
+  (unless (telnet? t)
+    (raise-argument-error 'telnet-send/gmcp "telnet?" t))
+  (unless (string? key)
+    (raise-argument-error 'telnet-send/gmcp "string?" key))
+  (unless (jsexpr? data)
+    (raise-argument-error 'telnet-send/gmcp "jsexpr?" data))
+  (telnet-send t (cons 'gmcp (format "~a ~a" key (jsexpr->string data)))))
 
-(struct telnet-settings (supports encoding to-utf8 from-utf8) #:mutable)
+(define (telnet-send/msdp t msdp-pair)
+  (unless (and (cons? msdp-pair)
+               (symbol? (car msdp-pair))
+               (jsexpr? (cdr msdp-pair)))
+    (raise-argument-error 'telnet-send/msdp "msdp?" msdp-pair))
+  (telnet-send t (cons 'msdp (msdp->bytes msdp-pair))))
 
-(struct telnet-user
-  ( cptr     ; int?              - cptr cast as uintptr_t for speed reasons
-    [th #:mutable]      ; 
-    settings; telnet-settings?
-    out     ; output-port?      - printing to here wraps it up and sends it to C backend
-    [conn? #:mutable]    ; boolean?          - is currently connected
-    )
-  #:property prop:output-port (struct-field-index out))
+(define (telnet-set-echo t on?)
+(unless (telnet? t)
+    (raise-argument-error 'telnet-set-echo "telnet?" t))
+  (telnet-send t (if on? 'echo 'noecho)))
 
 
+(struct telnet (cptr [on-message #:mutable] [client #:mutable] [term #:mutable] [encoding #:mutable] [supports #:mutable] [connected? #:mutable] [user-data #:mutable]))
+(define telnet?/c (struct/c telnet exact-positive-integer? (-> telnet-message?/c void?)  string? string? symbol? (listof symbol?) boolean? any/c))
 
-(define (new-connection cptr)
-  (define default-encoding 'ISO-8859-1)
-  (define default-settings
-    (telnet-settings '() default-encoding (get-converter #f default-encoding) (get-converter #t default-encoding)))
-  (telnet-user cptr
-               #f
-               default-settings
-               (make-output-port 'telnet-writer ; name
-                                 always-evt ; evt
-                                 (lambda (bytes start end non-block? breakable?) ; write-out, never blocks because the channel is non-blocking
-                                   (send-to-user cptr (bytes-convert/complete (subbytes bytes start end) (telnet-settings-from-utf8 default-settings)))
-                                   (- end start))
-                                 void)
-               #t
-               ))
+(define (telnet-supports? t option)
+  (unless (telnet? t)
+    (raise-argument-error 'telnet-supports? "telnet?" t))
+  (and (memq option (telnet-supports t)) #t))
 
+;(define (add-message-to-racket msg tn)
+;  (unless (telnet? tn)
+;    (raise-argument-error 'add-message-to-racket "telnet?" tn))
+;  (unless (telnet-message? msg)
+;    (raise-argument-error 'add-message-to-racket "telnet-message?" msg))
+;  (define enc (telnet-encoding tn))
+;  ((telnet-on-message tn) (transcode-telnet #t msg enc)))
+
+
+(define connection-map (make-hasheqv))
+
+(define (telnet-find-object cptr)
+  (unless (exact-nonnegative-integer? cptr)
+    (raise-argument-error 'telnet-find-object "#<CPTR:user_info_t>" cptr))
+  (hash-ref connection-map cptr (λ () #f)))
+
+(define (telnet-establish-connection tn)
+  (unless (telnet? tn)
+    (raise-argument-error 'establish-connection "telnet?" tn))
+  (define cptr (telnet-cptr tn))
+  (hash-set! connection-map cptr tn))
+
+(define (telnet-drop-connection tn)
+  (unless (telnet? tn)
+    (raise-argument-error 'establish-connection "telnet?" tn))
+  (telnet-send tn eof)
+  (define cptr (telnet-cptr tn))
+  (hash-remove! connection-map cptr))
+
+(define (add-message-to-griftos tn msg)
+  (unless (telnet? tn)
+    (raise-argument-error 'add-message-to-racket "telnet?" tn))
+  ((telnet-on-message tn) tn msg))
