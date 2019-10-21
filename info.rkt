b1fb9c2bc2bb86c739c7cba34c1aeb45af713cef
Dan Holtby <djholtby@gmail.com>
2019-10-21 13:23:28 -0400

    update info file

--- info.rkt
+++ info.rkt
@@ -1,4 +1,17 @@
 #lang info
 
 (define collection 'multi)
+(define pkg-name "racket-mud")
+(define pkg-authors '(djholtby))
 (define version "0.01")
+(define deps '("base"
+               "data-lib"
+               "db-lib"
+               "gregor-lib"
+               "libuuid"
+               "parser-tools-lib"
+               "rfc6455"
+               "versioned-box"
+               "web-server-lib"))
+(define build-deps '("racket-doc"
+                     "scribble-lib"))
