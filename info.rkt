018b09ee7629fd4cb443634502698742cae82b9d
Dan Holtby <djholtby@gmail.com>
2019-10-25 12:12:56 -0400

    change libuuid to uuid

--- info.rkt
+++ info.rkt
@@ -3,13 +3,12 @@
 (define collection 'multi)
 (define pkg-name "rackmud")
 (define pkg-authors '(djholtby))
-(define scribblings '(("rackmud/scribblings/rackmud.scrbl")))
 (define version "0.01")
-(define deps '("base"
+(define deps '("uuid"
+               "base"
                "data-lib"
                "db-lib"
                "gregor-lib"
-               "libuuid"
                "parser-tools-lib"
                "rfc6455"
                "versioned-box"
