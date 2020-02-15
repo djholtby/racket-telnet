5ddbdeee1b9226017e233a122e9810f631a93669
Dan Holtby <djholtby@gmail.com>
2020-02-15 14:19:12 -0500

    update deps

--- info.rkt
+++ info.rkt
@@ -3,6 +3,7 @@
 (define pkg-name "telnet")
 (define pkg-authors '(djholtby))
 (define version "1.00")
-(define deps '("base"))
+(define deps '("defconst"
+               "base"))
 (define build-deps '("racket-doc"
                      "scribble-lib"))
